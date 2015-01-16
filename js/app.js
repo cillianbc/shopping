75bd7757799a9e7fc176c9e3cbfb705a65b84476
Cillianbc <cillianbc@gmail.com>
2015-01-16 23:31:21 +1100

    Work done on getting jquery to work on key entry

--- js/app.js
+++ js/app.js
@@ -1,10 +1,34 @@
 $(document).ready(function () {
   $('ul').on('click', 'li', function (event) {
-  // fires when any LIs are clicked on
-  // including LIs that aren't on the page when it is initially loaded
   });
-  $("#add-items").keyup(function () {
-    var value = $(this).val();
-    $("#shopping-list li").add().text(value);
+
+//var shopItem = //Text entered in text box//
+//$("#shopping-list ul").append(shopItem);
+
+  $('button').on('click',function(){
+    $(this).remove('li:last');
+  });
+  $('#shopping-list li').on('click',function(){
+    $(this).addClass('li-strike');
+  });
+
+
+
+
+
+    $("input").keypress(function(event) {
+    if (event.which == 13) {
+        event.preventDefault();
+        $("form").submit();
+      }
+    $('input').submit(function() {
+    var value = $( this ).val();
+    $('input').submit(function() {
+    var value = $( this ).val();
+
+    $('#shopping-list').append("<li>"+value+"</li>");
+    $('#shopping-list').css("visibility","visible");
     });
+    });
+  });
 });
